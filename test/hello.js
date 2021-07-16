726fb48c71b8247b0f30464ce4ec7ee4b2b97c47
agunacoco <agunaco3994@gmail.com>
2021-07-16 20:52:09 +0900

    agunacoco

--- test/hello.js
+++ test/hello.js
@@ -1,3 +1,26 @@
+// Component는 new Vue()앞에 정의.
+Vue.component('agunacoco', { // component 전역 등록
+    props: ['name'], //매개변수 역할, 데이터를 받아온다. 
+    template: '<p>{{message}}</p>', //필수 요소. 최상위 태그는 하나만 있어야한다. 그 안에는 여러 개 있어도 상관없다.
+    data: function () { //객체가 아닌 함수인 이유는 html에서 호출될 때마다 돌아가야하니깐 함수로 표현.
+        return { message: 'hello!, ' + this.name }
+    }
+});
+
+Vue.component('add', {
+    template: '<button @click="add">{{ count }}</button>',
+    data() {
+        return { count: 0 }
+    },
+    methods: {
+        add() {
+            this.count += 1
+            //this.$parent.$data.totalCount += 1
+            this.$emit('event-add', 1);  //
+        }
+    }
+});
+
 let app = new Vue({
     el: '#app',
     data: {
@@ -13,9 +36,10 @@ let app = new Vue({
         ],
         user: {
             title: "저를 소개합니다!",
-            userId: 0416,
+            userId: "aaa0416",
             userName: 'agunacoco',
-            email: 'agunaco3994@gmail'
+            email: 'agunaco3994@gmail',
+            age: 20
         },
         counter1: 0,
         counter2: 0,
@@ -32,10 +56,11 @@ let app = new Vue({
         lazytest: {
             value: 'hello',
             value2: 'hello'
-        }
-
+        },
+        message: '',
+        totalCount: 0,
     },
-    methods: {
+    methods: { // 함수, html에서 함수를 호출할때는 뒤에 ()를 써야한다.
         addCounter() {
             this.counter2 += 1
         },
@@ -44,6 +69,29 @@ let app = new Vue({
         },
         sayHi(name) {
             this.paratest.message = 'Hi, ' + name
+        },
+        updateTotal(count) {
+            this.totalCount += count
+        }
+    },
+    // method와 computed의 차이는 캐싱.
+    // computed는 캐싱된 값으로 사용하게 된다. 계산된 값을 리턴한다.
+    computed: { // 중복제거 효과도 있다. 
+        user_info() {
+            return this.user.userId + '(' + this.user.userName + ')'
+        },
+        age_message() {
+            let str = "Adult"
+            if (this.user.age < 20) {
+                str = 'Not Adult'
+            }
+            return str
+        }
+    },
+    components: {  // component 지역등록
+        'aguna': {
+            template: '<p>This is local component</p>'
         }
     }
 });
+
---
+++ test/test.js
@@ -0,0 +1,38 @@
+Vue.component('agunacoco', {
+    template: `<div>
+                    <h2>글쓰기</h2>
+                    <label for="name">작성자</label>
+                    <input type="text" v-model="userId">
+                    <br>
+                    <label for="content">내용</label>
+                    <textarea type="text" v-model="content"></textarea>
+                    <br>
+                    <button @click="addContent">등록</button>
+                </div>`,
+    data() { //객체가 아닌 함수인 이유는 html에서 호출될 때마다 돌아가야하니깐 함수로 표현.
+        return {
+            userId: null,
+            content: null,
+            message: []
+        }
+    },
+    methods: {
+        addContent() {
+            const message = {
+                userId: this.userId,
+                content: this.content
+            }
+            this.message.unshift(message)
+            this.userId = null
+            this.content = null
+        },
+    }
+});
+
+let app = new Vue({
+    el: '#app',
+    data: {
+    },
+    methods: {
+    },
+});
