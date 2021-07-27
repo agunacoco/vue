f611355b6cdf69827fbc01e2717e54f49674d458
agunacoco <agunaco3994@gmail.com>
2021-07-27 22:27:56 +0900

    agunacoco

--- my-cafe/src/components/Menus.vue
+++ my-cafe/src/components/Menus.vue
@@ -15,8 +15,8 @@ export default {
   data() {
     return {
       menus: [
-        { id: 1, name: "아메리카노", price: 1000 },
-        { id: 2, name: "아메리카노", price: 3000 },
+        { id: 1, name: "아이스 아메리카노", price: 1000 },
+        { id: 2, name: "블루레몬에이드", price: 3000 },
         { id: 3, name: "제주유기농말차프라프치노", price: 6000 },
       ],
     };
